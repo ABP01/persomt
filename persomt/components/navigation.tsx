6e19f0b9bb0ee5d958fd76b1d7bcfc95f232218b
Armel Bogue <komlanbogue@gmail.com>
2024-12-25 18:47:23 +0000

    add welcome message component; integrate user greeting and loading states in header

--- persomt/components/navigation.tsx
+++ persomt/components/navigation.tsx
@@ -66,7 +66,7 @@ export const Navigation = () => {
                         <Button
                             variant={route.href === pathname ? "secondary" : "ghost"}
                             key={route.href}
-                            onClick={() => onClick(route.href)}>
+                            onClick={() => onClick(route.href)} className="w-full justify-start">
                             {route.label}
                             </Button>
                          
