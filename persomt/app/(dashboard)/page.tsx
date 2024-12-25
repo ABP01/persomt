117c5a8974e873e56a0f2217ff73c3f60e57ff12
Armel Bogue <komlanbogue@gmail.com>
2024-12-25 15:36:07 +0000

    add Hono API routes and integrate UserButton in dashboard

--- persomt/app/(dashboard)/page.tsx
+++ persomt/app/(dashboard)/page.tsx
@@ -1,8 +1,13 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 
 export default function Home() {
   return (
+    <>
     <p className="bg-slate-600">This is an authenticated route</p>
+    <UserButton />
+    </>
+
   );
 }
